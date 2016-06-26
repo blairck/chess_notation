2dd6da12b9bdd77264062dfc0219dbb1f52fe925
Chris Blair <chris.blair36@gmail.com>
2016-06-25 21:26:47 -0500

    Add main loop logic to quiz the user with random games/orientations while timing the trials

--- src/main.py
+++ src/main.py
@@ -1,15 +1,30 @@
 """Where player runs the game"""
 
-from src import board
+import random
+import time
+
+import board
+import conversion
+import games
 
 if __name__ == '__main__':
-    BOARD_DESCRIPTION = ("rnbqkbnr",
-                         "pppppppp",
-                         "        ",
-                         "        ",
-                         "        ",
-                         "        ",
-                         "PPPPPPPP",
-                         "RNBQKBNR",)
-    BOARD = board.Board(BOARD_DESCRIPTION, orientation=True)
-    print(BOARD)
+    NUMBER_OF_TRIALS = 1
+    for i in range(NUMBER_OF_TRIALS):
+        X_LOC_CHESS, Y_LOC_CHESS = board.identify_random_square()
+        LOCATION = conversion.coordinate_to_alg(X_LOC_CHESS, Y_LOC_CHESS)
+        ORIENTATIONS = (True, False)
+        GAMES = games.GamePositions()
+        BOARD_DESCRIPTION = GAMES.random_game()
+        BOARD = board.Board(BOARD_DESCRIPTION,
+                            orientation=random.choice(ORIENTATIONS))
+        BOARD.highlight_square(X_LOC_CHESS, Y_LOC_CHESS)
+        BOARD.update_board_string()
+        print(BOARD)
+        start = time.time()
+        ATTEMPT = input('Enter highlighted square: ')
+        end = time.time()
+        if ATTEMPT == LOCATION:
+            print("Correct!")
+        else:
+            print("Wrong, the answer was {0}".format(LOCATION))
+        print("Attempt took {0} seconds".format(end - start))
