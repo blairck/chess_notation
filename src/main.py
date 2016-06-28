b7ef85c934be247f0d84e189a159d2324bd64e97
Chris Blair <chris.blair36@gmail.com>
2016-06-27 22:37:31 -0500

    Incorporate file record logic to main play loop

--- src/main.py
+++ src/main.py
@@ -6,7 +6,9 @@ import time
 import board
 import conversion
 import games
-from settings import RECORD_FILE
+from settings import (NUMBER_OF_TRIALS,
+                      RECORD_FILE,
+                      SAVE_PROGRESS)
 
 def write_record_to_file(a_string, file_name):
     with open(file_name, 'w') as f:
@@ -18,8 +20,12 @@ def get_record_from_file(file_name):
     return result
 
 if __name__ == '__main__':
-    NUMBER_OF_TRIALS = 1
     for i in range(NUMBER_OF_TRIALS):
+        # Setup some initial variables
+        record_total_time = 0.0
+        record_total_trials = 0
+
+        # Display the board
         X_LOC_CHESS, Y_LOC_CHESS = board.identify_random_square()
         LOCATION = conversion.coordinate_to_alg(X_LOC_CHESS, Y_LOC_CHESS)
         ORIENTATIONS = (True, False)
@@ -31,10 +37,41 @@ if __name__ == '__main__':
         BOARD.update_board_string()
         print(BOARD)
         start = time.time()
-        ATTEMPT = input('Enter highlighted square: ')
+
+        # Display status information
+        if SAVE_PROGRESS:
+            try:
+                # Try to retrieve data from record file
+                record = get_record_from_file(RECORD_FILE).split(',')
+                record_total_time = float(record[0])
+                record_total_trials = int(record[1])
+                template = "Average time: {0:.2f}\nTotal seconds: {1:.2f}, \
+Total trials: {2}"
+                trial_average = record_total_time/float(record_total_trials)
+                print(template.format(trial_average, 
+                                      record_total_time,
+                                      record_total_trials))
+            except FileNotFoundError:
+                print("No record file found.\n\
+Creating a new one after this trial: {0}".format(RECORD_FILE))
+
+        # Ask for input from the user
+        ATTEMPT = input('Enter highlighted square, or \'quit\': ')
         end = time.time()
         if ATTEMPT == LOCATION:
-            print("Correct!")
+            print("CORRECT!!")
+        elif ATTEMPT == "quit" or ATTEMPT == "q":
+            print("Exiting, current trial will not be saved.")
+            break
         else:
             print("Wrong, the answer was {0}".format(LOCATION))
-        print("Attempt took {0} seconds".format(end - start))
+        TRIAL_DURATION = end - start
+        print("Attempt took {0:.03f} seconds".format(TRIAL_DURATION))
+
+        # Save results of this trial to record file
+        if SAVE_PROGRESS:
+            record_total_time = record_total_time + TRIAL_DURATION
+            record_total_trials = record_total_trials + 1
+            write_record_to_file("{0},{1}".format(record_total_time,
+                                                  record_total_trials),
+                                 RECORD_FILE)
